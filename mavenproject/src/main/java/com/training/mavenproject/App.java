cca9461ff64c88715cdcc0c30f9b2255a69bab69
Suryaprakash.C <Suryaprakash.C@EXIND3020-86.allfon.in>
2022-04-29 17:25:57 +0530

    30

---
+++ mavenproject/src/main/java/com/training/mavenproject/App.java
@@ -0,0 +1,15 @@
+package com.training.mavenproject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	Book book = new Book("java",411);
+    	System.out.println(book);
+    	
+    }
+}
---
+++ mavenproject/src/main/java/com/training/mavenproject/Book.java
@@ -0,0 +1,33 @@
+package com.training.mavenproject;
+
+public class Book {
+	
+	private String bookName;
+	private int bookNumber;
+	public Book() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public Book(String bookName, int bookNumber) {
+		super();
+		this.bookName = bookName;
+		this.bookNumber = bookNumber;
+	}
+	public String getBookName() {
+		return bookName;
+	}
+	public void setBookName(String bookName) {
+		this.bookName = bookName;
+	}
+	public int getBookNumber() {
+		return bookNumber;
+	}
+	public void setBookNumber(int bookNumber) {
+		this.bookNumber = bookNumber;
+	}
+	@Override
+	public String toString() {
+		return "Book [bookName=" + bookName + ", bookNumber=" + bookNumber + "]";
+	}
+
+}

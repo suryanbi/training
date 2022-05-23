6f48655779d415ca7af0b23e6abdd8d9cd85e8e4
Suryaprakash.C <Suryaprakash.C@EXIND3020-86.allfon.in>
2022-05-23 17:48:12 +0530

    eureka micro service two

---
+++ eureka_micro_service_two/src/main/java/com/example/demo/model/DriverDTO.java
@@ -0,0 +1,20 @@
+package com.example.demo.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class DriverDTO {
+	
+	private int driverId;
+	
+	private String driverName;
+	
+	private long mobileNumber;
+	
+	private double rating;
+
+}
